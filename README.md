16d5e588e8eca21fb3dbcf94a24b17dc9b9d85c9
BG8CFB <2945362558@qq.com>
2021-12-07 18:08:23 +0800

    插件安装完成

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-# templatevue3
+# TemplateVue3
+
+开发模板：
+
+
+
+插件：
+
+```
+UI插件：ant-design-vue
+绘图插件：vue3-echarts
+状态管理插件：pinia
+路由管理插件：vue-router
+AJAX插件：axios
+CSS预编译插件：less，less-loader
+```
+
+
+
+```
+开发库 Vue/cli4 Vue3.2 TypeScript  eslint WebPack
+```
 
 ## Project setup
 ```
---
+++ vue.config.js
@@ -0,0 +1,27 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { resolve } = require('path');
+
+module.exports = {
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('assets', resolve('src/assets'))
+      .set('components', resolve('src/components'))
+      .set('base', resolve('baseConfig'))
+      .set('public', resolve('public'));
+  },
+  devServer: {
+    open: true, // 是否开启浏览器
+    host: '', // 主机IP
+    port: 9000, // 端口号
+    // proxy: {
+    //   '/api': {
+    //     target: '',
+    //     changeOrigin: true,
+    //     pathRewrite: {
+    //       '/api': ''
+    //     }
+    //   }
+    // }
+  },
+};
