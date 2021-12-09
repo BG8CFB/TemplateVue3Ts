45c51a33598093a3f8f66220b498e061145b1528
BG8CFB <2945362558@QQ.COM>
2021-12-09 23:52:38 +0800

    环境更新

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,20 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { defineConfig } = require('@vue/cli-service');
 const { resolve } = require('path');
 
-module.exports = {
+module.exports = defineConfig({
+  transpileDependencies: true,
   chainWebpack: (config) => {
     config.resolve.alias
-      .set('@', resolve('src'))
-      .set('assets', resolve('src/assets'))
-      .set('components', resolve('src/components'))
-      .set('base', resolve('baseConfig'))
-      .set('public', resolve('public'));
+    .set('@', resolve('src'))
+    .set('assets', resolve('src/assets'))
+    .set('components', resolve('src/components'))
+    .set('public', resolve('public'));
   },
   devServer: {
     open: true, // 是否开启浏览器
     host: '', // 主机IP
     port: 9000, // 端口号
+    // // 代理配置
     // proxy: {
     //   '/api': {
     //     target: '',
@@ -24,4 +25,4 @@ module.exports = {
     //   }
     // }
   },
-};
+});
